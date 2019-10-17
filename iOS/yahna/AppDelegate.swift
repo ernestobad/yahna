46603973badfa1e34e32da9d7a5dfae9b5d345a3
Ernesto Badillo <edmfer@microsoft.com>
2019-10-17 08:02:35 -0700

    Commenting out Db initialization

--- iOS/yahna/AppDelegate.swift
+++ iOS/yahna/AppDelegate.swift
@@ -13,14 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        do {
-            try DatabaseManager.shared.createDB()
-        } catch {
-            Log.logger.error("Error creationg the DB: \(error)")
-            return false
-        }
+//        do {
+//            try DatabaseManager.shared.createDB()
+//        } catch {
+//            Log.logger.error("Error creationg the DB: \(error)")
+//            return false
+//        }
         
-        // Override point for customization after application launch.
         return true
     }
 
